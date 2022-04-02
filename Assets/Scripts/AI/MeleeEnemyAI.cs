f7aff1681bafb7f77a0d77b3270a6578a9d91433
Ruud Cremers <ruud88@gmail.com>
2022-04-02 21:00:00 +0200

    variants

--- Assets/Scripts/AI/MeleeEnemyAI.cs
+++ Assets/Scripts/AI/MeleeEnemyAI.cs
@@ -4,12 +4,63 @@ namespace LD50.Scripts.AI
 {
     public class MeleeEnemyAI : BaseEnemyAI 
     {   
+        [SerializeField]
+        protected float _meleeRange = 1f; 
+
+        protected float _attackCooldownRemaining = 0f;
+
+        protected MeleeAttack _meleeAttack;
+        
+        [SerializeField] 
+        private float _baseAttackCooldown = 1f;
+        private float _attackCooldownModifier = 1f;
+
+        protected override void Start()
+        {
+            base.Start();
+            _meleeAttack = GetComponent<MeleeAttack>();
+        }
+
+        protected override void Update()
+        {                        
+            base.Update();
+            UpdateTimers();
+        }
+        
         protected override void AgroBehavior()
         {
-            var relativeVector = _target.position - transform.position;
-            _moveDirection = relativeVector.normalized;
-            RigidBody.velocity = _moveDirection * _wanderSpeed;
+            if (_target == null) return;
+            // If close enough to target, swing
+            var distanceToTarget = Vector2.Distance(_target.position, transform.position);
+            if (distanceToTarget < _meleeRange)
+            {
+                if (_attackCooldownRemaining <= 0f) 
+                {
+                    RigidBody.velocity = Vector2.zero;
+                    _meleeAttack.Fire(_moveDirection);
+                    _attackCooldownRemaining = _baseAttackCooldown * _attackCooldownModifier;
+                }
+            } 
+            // Move closer to target
+            else 
+            {
+                var relativeVector = _target.position - transform.position;
+                _moveDirection = relativeVector.normalized;
+                RigidBody.velocity = _moveDirection * _wanderSpeed;
+            }
         }
+
+        private void UpdateTimers ()
+        {
+            if (_attackCooldownRemaining > 0f)
+            {
+                _attackCooldownRemaining -= Time.deltaTime;
+                if (_attackCooldownRemaining < 0f)
+                {
+                    _attackCooldownRemaining = 0f;
+                }
+            }
+        }  
     }
 }
 
