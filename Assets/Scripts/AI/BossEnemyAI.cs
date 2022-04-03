01040d7a572a81c01868075dd5be02aa2b1961a7
Ruud Cremers <ruud88@gmail.com>
2022-04-03 21:55:34 +0200

    flappy bird

--- Assets/Scripts/AI/BossEnemyAI.cs
+++ Assets/Scripts/AI/BossEnemyAI.cs
@@ -14,12 +14,16 @@ namespace LD50.Scripts.AI
 
         private float _moveSpeedModifier = 1f;
 
+        private Vector2 _playerDirection;
+        protected RangedAttack _rangedAttack;
+
         public Damagable HitpointData;
         
         [SerializeField] 
         private float _baseAttackCooldown = 1f;
         private float _attackCooldownModifier = 1f;
 
+        [SerializeField]
         private float _distanceDifficultyModifier = 1f;
 
         private bool _phase4Transition = false;
@@ -27,6 +31,7 @@ namespace LD50.Scripts.AI
         protected override void Start()
         {
             base.Start();
+            _rangedAttack = GetComponent<RangedAttack>();
             _meleeAttack = GetComponent<MeleeAttack>();
         }
 
@@ -35,6 +40,11 @@ namespace LD50.Scripts.AI
             // There should always be a friendly target left, else it's game over
             _target = UnitManager.GetClosestFriendly(transform.position);
             _distanceToTarget = Vector2.Distance(_target.position, transform.position);
+            _distanceDifficultyModifier = 1f + (Vector2.Distance(PlayerController.Instance.transform.position, transform.position) / 30f);
+
+            var relativeVector = PlayerController.Instance.transform.position - transform.position;
+            _playerDirection = relativeVector.normalized;
+
             _seekTime = 0.2f;
         }
 
@@ -49,47 +59,63 @@ namespace LD50.Scripts.AI
         {
             if (HitpointData.HealthPercentage > 0.9f) 
             {
-                Phase1Mechanics();
+                Phase1Mechanics(); // 100-90%
             } 
-            else if (HitpointData.HealthPercentage > 0.7f)
-            {
-
-            } 
-            else if (HitpointData.HealthPercentage > 0.5f)
-            {
-
+            else if (HitpointData.HealthPercentage > 0.65f)
+            {                
+                Phase2Mechanics(); // 65-90%
             } 
             else if (HitpointData.HealthPercentage > 0.1f)
             {
-
+                Phase3Mechanics(); // 70-10%
             } 
+            else if (HitpointData.HealthPercentage > 0.03f)
+            {
+                Phase4Mechanics(); // (buffed) 100-40%
+            }
+            else 
+            {
+                Phase5Mechanics(); // (buffed) 40-0%
+            }  
         }
 
         private void Phase2Mechanics()
         {
+            // Boss will continue his Phase 1 mechanics, but also shoot a lot of piercing bullets
+            Phase1Mechanics();
 
+            _rangedAttack.Fire(_playerDirection);
+            var quat = Quaternion.Euler(0f, 0f, 30f);
+            _rangedAttack.Fire(quat * _moveDirection);
         }
 
         private void Phase3Mechanics()
         {
-            
+            // Boss will actively try to hinder the player by spawning deathzones in their path
         }
 
         private void Phase4Mechanics()
         {
+            // Boss gets tankier, vastly increasing his effective health
+
             if (!_phase4Transition) 
             {
-                // Boss gets tankier, vastly increasing his effective health
-                HitpointData.MaxHealth = HitpointData.MaxHealth * 8;
-                HitpointData.Health = HitpointData.Health * 8;
+                HitpointData.MaxHealth = HitpointData.MaxHealth * 10;
+                HitpointData.Health = HitpointData.Health * 10;
             }
+        }
+
+        private void Phase5Mechanics()
+        {
+            // Boss will go berserk and unleash a bullet hell 
         }
 
         private void Phase1Mechanics()
         {
+            // Phase1: Boss simply acts as a strong melee enemy that scales with distance from player
             if (_target == null) 
             {
-                _state = EnemyCombatState.Idle;
+                _target = UnitManager.GetClosestFriendly(transform.position);
                 _currentAgroRange = _agroRange;
                 return;
             }  
