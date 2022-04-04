63f8e019451676456da2a5337f3e9d2ee3eb21a2
Ruud Cremers <ruud88@gmail.com>
2022-04-04 18:16:44 +0200

    balance

--- Assets/Scripts/AI/BaseEnemyAI.cs
+++ Assets/Scripts/AI/BaseEnemyAI.cs
@@ -15,7 +15,7 @@ namespace LD50.Scripts.AI
         [SerializeField]
         private int ExpWorth;
 
-        protected float _expMultiplier = 1f;
+        protected float _expMultiplier = 1.2f;
 
         protected EnemyCombatState _state = EnemyCombatState.Idle;
 
